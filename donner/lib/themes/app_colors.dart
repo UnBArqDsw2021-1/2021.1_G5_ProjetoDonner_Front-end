9b2654ec3f262b8dd28ccb5e34d32aa0f04fe990
kleidson-alves <kleidsonalves15@gmail.com>
2021-09-14 12:18:49 -0300

    Criaçao do tema da aplicaçao
    Co-authored-by: Lucas Gabriel       <lucasbsb100@gmail.com>
    Co-authored-by: Wellington Jonathan <wellpriz@gmail.com>

---
+++ donner/lib/themes/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primary = Color(0xFFC87ED3);
+  static final secondary = Color(0xFF00ACFF);
+  static final terciary = Color(0xFFFF4A6D);
+  static final backgroundColor = Color(0xFFFFFFFF);
+  static final textTitleColor = Color(0xFF242423);
+  static final bodyTextColor = Color(0xFF585857);
+  static final stroke = Color(0xFFC5C5C5);
+  static final inputTextColor = Color(0xFF9D9CA4);
+  static final headerTextColor = Color(0xFF242423);
+}
