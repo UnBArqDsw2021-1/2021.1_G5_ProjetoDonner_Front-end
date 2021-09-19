1ee14cccf6c76deffe07d85590c9c8cc38aa633f
kleidson-alves <kleidsonalves15@gmail.com>
2021-09-19 19:09:35 -0300

    Correcao da aplicacao do adapter
    Co-authored-by: Lucas Gabriel       <lucasbsb100@gmail.com>
    Co-authored-by: Lucas Rodrigues     <lucasro229@gmail.com>
    Co-authored-by: Hugo Bezerra        <hugoricardo.so.be@gmail.com>

--- donner/lib/widgets/models/client_model.dart
+++ donner/lib/widgets/models/client_model.dart
@@ -1,62 +1,26 @@
 import 'package:donner/widgets/models/user_model.dart';
 
 class ClientModel extends User {
-  String _phone;
-  String _photoUrl;
-  String _description;
-  List<dynamic> _announcements;
+  String phone;
+  String photoUrl;
+  String description;
+  List<dynamic> announcements;
 
   ClientModel({
-    required String phone,
-    required String photoUrl,
-    required String description,
-    required List<dynamic> announcements,
+    required this.phone,
+    required this.photoUrl,
+    required this.description,
+    required this.announcements,
     required String name,
     required String email,
-  })  : _phone = phone,
-        _photoUrl = photoUrl,
-        _description = description,
-        _announcements = announcements,
-        super(name, email);
-
-  String getPhone() {
-    return _phone;
-  }
-
-  void setPhone(String phone) {
-    _phone = phone;
-  }
-
-  String getPhotoUrl() {
-    return _photoUrl;
-  }
-
-  void setPhotoUrl(String photoUrl) {
-    _photoUrl = photoUrl;
-  }
-
-  String getDescription() {
-    return _description;
-  }
-
-  void setDescription(String description) {
-    _description = description;
-  }
-
-  List<dynamic> getAnnoucements() {
-    return _announcements;
-  }
-
-  void setAnnoucements(List<dynamic> announcements) {
-    _announcements = announcements;
-  }
+  }) : super(name, email);
 
   Map<String, dynamic> toMap() {
     return {
-      '_phone': _phone,
-      '_photoUrl': _photoUrl,
-      '_description': _description,
-      '_announcements': _announcements,
+      'phone': phone,
+      'photoUrl': photoUrl,
+      'description': description,
+      'announcements': announcements,
     };
   }
 
@@ -64,52 +28,14 @@ class ClientModel extends User {
     return ClientModel(
       name: map['name'],
       email: map['email'],
-      phone: map['_phone'],
-      photoUrl: map['_photoUrl'],
-      description: map['_description'],
-      announcements: List.from(map['_announcements']),
+      phone: map['phone'],
+      photoUrl: map['photoUrl'],
+      description: map['description'],
+      announcements: List.from(map['announcements']),
     );
   }
   @override
   String toString() {
-    return 'ClientModel(_name: ${getName()},_email: ${getEmail()} ,_phone: $_phone, _photoUrl: $_photoUrl, _description: $_description, _annoucements: $_announcements)';
+    return 'ClientModel(name: $name, email: $email, phone: $phone, photoUrl: $photoUrl, description: $description, announcements: $announcements)';
   }
-
-// ClientModel copyWith({
-  //   String? _phone,
-  //   String? _photoUrl,
-  //   String? _description,
-  //   List? _annoucements,
-  // }) {
-  //   return ClientModel(
-  //     _phone: _phone ?? this._phone,
-  //     _photoUrl: _photoUrl ?? this._photoUrl,
-  //     _description: _description ?? this._description,
-  //     _annoucements: _annoucements ?? this._annoucements,
-  //   );
-  // }
-
-  // String toJson() => json.encode(toMap());
-
-  // factory ClientModel.fromJson(String source) => ClientModel.fromMap(json.decode(source));
-
-  // @override
-  // bool operator ==(Object other) {
-  //   if (identical(this, other)) return true;
-  //   final listEquals = const DeepCollectionEquality().equals;
-
-  //   return other is ClientModel &&
-  //     other._phone == _phone &&
-  //     other._photoUrl == _photoUrl &&
-  //     other._description == _description &&
-  //     listEquals(other._annoucements, _annoucements);
-  // }
-
-  // @override
-  // int get hashCode {
-  //   return _phone.hashCode ^
-  //     _photoUrl.hashCode ^
-  //     _description.hashCode ^
-  //     _annoucements.hashCode;
-  // }
 }
--- donner/lib/widgets/models/user_model.dart
+++ donner/lib/widgets/models/user_model.dart
@@ -1,22 +1,6 @@
 abstract class User {
-  String _name;
-  String _email;
+  String name;
+  String email;
 
-  User(this._name, this._email);
-
-  void setName(String name) {
-    _name = name;
-  }
-
-  String getName() {
-    return _name;
-  }
-
-  void setEmail(String email) {
-    _email = email;
-  }
-
-  String getEmail() {
-    return _email;
-  }
+  User(this.name, this.email);
 }
